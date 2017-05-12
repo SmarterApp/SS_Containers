3a9530d8648e8fc1928e2533a8ec1f5c4f812b85
wtritch <wtritchler@fairwaytech.com>
2017-05-12 14:49:40 -0700

    Minor cleanup

--- redis-image/src/main/docker/sentinel_check.sh
+++ redis-image/src/main/docker/sentinel_check.sh
@@ -13,24 +13,6 @@ function isSentinelRunning() {
     fi
 }
 
-function getMasterAddress() {
-    local __resultvar=$1
-    local master=$(redis-cli -p ${REDIS_SENTINEL_SERVICE_PORT} --csv SENTINEL get-master-addr-by-name mymaster | tr ',' ' ' | cut -d' ' -f1)
-    if [[ -n ${master} ]]; then
-        eval $__resultvar="${master//\"}"
-    fi
-}
-
-function isMasterDown() {
-    local __resultvar=$1
-    redis-cli -h ${masterAddr} INFO > /dev/null
-    if [[ "$?" == "0" ]]; then
-        eval $__resultvar=false
-    else
-        eval $__resultvar=true
-    fi
-}
-
 #Reset the current state to remove stale sentinels and slaves
 redis-cli -p ${REDIS_SENTINEL_SERVICE_PORT} sentinel reset mymaster
 
@@ -40,19 +22,5 @@ if [[ "$isRunning" == false ]]; then
     echo "Sentinel is not running"
     exit 1
 fi
-
-#getMasterAddress masterAddr
-#echo "Master addr: $masterAddr"
-#if [[ -z ${masterAddr} ]]; then
-#    echo "No master found"
-#    exit 1
-#fi
-#
-#isMasterDown masterDown
-#echo "Is master down: $masterDown"
-#if [[ "$masterDown" == true ]]; then
-#    echo "Redis Sentinel is unable to contact Master"
-#    exit 1
-#fi
 
 exit 0
